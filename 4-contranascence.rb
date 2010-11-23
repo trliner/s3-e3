89113d805010a43ab1c30ea8d94e3d796327d291
Tim <trliner@gmail.com>
2010-11-22 23:54:15 -0600

    contranascence example

---
+++ 4-contranascence.rb
@@ -0,0 +1,33 @@
+class Apartment
+  attr_accessor :name, :rent, :bedrooms, :allow_pets, :allow_felonies,
+    :commission, :neighborhood_id
+
+# the values in this hash must all be distinct otherwise the neighborhood
+# method wouldn't work as expected
+  NEIGHBORHOODS = {
+    :downtown => 1,
+    :uptown => 2,
+    :east_side => 3,
+    :hyde_park => 4
+  }
+
+  def initialize(name, opts)
+    @name = name
+    @rent = opts[:rent]
+    @bedrooms = opts[:bedrooms]
+    @allow_pets = opts[:allow_pets] != false
+    @allow_felonies = opts[:allow_felonies] || false
+    @commission = opts[:commission] || 100
+    @neighborhood_id = opts[:neighborhood_id]
+  end
+
+  def commission_check
+    rent * (commission / 100.to_f)
+  end
+
+  def neighborhood
+    neighborhood = NEIGHBORHOODS.invert[neighborhood_id].to_s.
+      split('_').collect{|s| s.capitalize}.join(" ")
+    neighborhood.empty? ? "None Specified" : neighborhood
+  end
+end
